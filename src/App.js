5c368457090bc44f2370d9897d036e4976662744
RaghaviKannan <ragzagnes@gmail.com>
2023-09-09 20:47:27 +0530

    download added

--- src/App.js
+++ src/App.js
@@ -6,53 +6,58 @@ import { useFormik } from 'formik';
 
 function App() {
   const [qrcode, setQrcode] = useState("")
-  const [submitted,setSubmitted] = useState(false)
+  const [submitted, setSubmitted] = useState(false)
   const formik = useFormik(
     {
-      initialValues:{
-        data:"",
-        width:"",
-        height:"",
-        qrcolor:"",
-        bgcolor:"",
-        format:""
+      initialValues: {
+        data: "",
+        width: "",
+        height: "",
+        qrcolor: "",
+        bgcolor: "",
+        format: ""
       },
-      validate:({data,width,height,format})=>{
+      validate: ({ data, width, height, format }) => {
         const err = {}
-          if(!data){
-            err.data = "Enter a data"
-          }
-          if(width < 100 || height < 100){
-            err.size = "Enter a value of 100 or more"
-          }
-          if(height != width){
-            err.size = "Width and Height values should be equal"
-          }
-          if(format === ""){
-            err.format = "Please select a format"
-          }
+        if (!data) {
+          err.data = "Enter a data"
+        }
+        if (width < 100 || height < 100) {
+          err.size = "Enter a value of 100 or more"
+        }
+        if (height != width) {
+          err.size = "Width and Height values should be equal"
+        }
+        if (format === "") {
+          err.format = "Please select a format"
+        }
         return err
       },
-      onSubmit:async ({data,width,height,qrcolor,bgcolor,format})=>{
-        
+      onSubmit: ({ data, width, height, qrcolor, bgcolor, format }) => {
+
         const qrURL = `http://api.qrserver.com/v1/create-qr-code/?data=${data}&size=${width}x${height}&color=${qrcolor.slice(1)}&bgcolor=${bgcolor.slice(1)}&format=${format}`
         setQrcode(qrURL)
       }
     }
   )
-  const downloadQRCode=()=>{
-    const link = document.createElement('a')
-    link.href = qrcode
-    link.download = `qrcode.${formik.values.format}`
-
-    link.click()
+  const downloadQRCode = async () => {
+    try {
+      const result = await fetch(qrcode)
+      const file = await result.blob()
+      const link = document.createElement('a')
+      link.href = URL.createObjectURL(file)
+      link.download = `qrcode.${formik.values.format}`
+      link.click()
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <div className="App">
       <h1>QR Code Generator</h1>
       <div className='container'>
-        <form onSubmit={(e)=>{e.preventDefault(); setSubmitted(true); formik.handleSubmit()}}>
+        <form onSubmit={(e) => { e.preventDefault(); setSubmitted(true); formik.handleSubmit() }}>
           <div className='row'>
             <div className='col-lg-6'>
               <h4>Details</h4>
@@ -74,7 +79,7 @@ function App() {
               {submitted && formik.errors.size ? <span style={{ color: "red" }}>{formik.errors.size}</span> : null}
               <div className='row'>
                 <label style={{ padding: 0 }}>QR Color: </label>
-                <input name="qrcolor"type={'color'} onChange={formik.handleChange} value={formik.values.qrcolor} className={'form-control'} style={{ padding: 0 }} />
+                <input name="qrcolor" type={'color'} onChange={formik.handleChange} value={formik.values.qrcolor} className={'form-control'} style={{ padding: 0 }} />
               </div>
               <div className='row'>
                 <label style={{ padding: 0 }}>Background Color: </label>
